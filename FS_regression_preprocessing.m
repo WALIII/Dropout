9fe2ca05ac8f0113d3a0d79ce4035caf663aec95
WALIII <bliberti@bu.edu>
2018-07-29 16:27:58 -0400

    update indexing again

--- FS_regression_preprocessing.m
+++ FS_regression_preprocessing.m
@@ -69,8 +69,8 @@ pad_s = pad*48000 % samples
 
 tv_song_r = pad:48000:max_dat/30;
 % Song start and end
-s_st = (align/30)*48000)-pad_s;
-s_end =  (align/30)*48000)+ (max_dat/30)*48000 + pad_s;
+s_st = ((align/30)*48000)-pad_s;
+s_end =  ((align/30)*48000)+ (max_dat/30)*48000 + pad_s;
 
 %to do ( get the song data out!)
 data{i}.Motif_ind = Motif_ind;
@@ -82,7 +82,7 @@ end
 
 
 for i = 1:size(data)
-  if i = 1;
+  if i == 1;
 
 
 % concat calcium and song data ( and keep motif data )
@@ -91,7 +91,8 @@ for i = 1:size(data)
 
 
 FS_ROI_Data = data;
- end
+  end
+end
 
 % check song data
 
