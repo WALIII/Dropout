f6bc9383a685392bbafdd92a806cd07ae805f7de
WALIII <bliberti@bu.edu>
2018-07-29 18:16:15 -0700

    warp regression update

--- WarpRegress.m
+++ WarpRegress.m
@@ -14,8 +14,6 @@ for i = 1:100%size(D.song_r,1)
         [song_start, song_end, score_d(counter,:)] = find_audio(D.song_r(i,:)', template, fs, 'match_single', true,'constrain_length', 0.25);
         [WARPED_TIME{counter} WARPED_audio(:,counter)]  = warp_audio(D.song_r(i,song_start*fs:song_end*fs), template, fs,[]);
         
-       D2.song_dsw(counter,:) = downsample(zftftb_rms(WARPED_audio(0.25*fs:end,counter),48000),1000);
-
         idex(:,counter) = i; % trial number
         disp(['Finished warping ' num2str(counter), 'of ', num2str(size(D.song_r,1))]);
       counter = counter+1;
@@ -31,6 +29,19 @@ disp(' Sorting data...')
         D2.unsorted = D.unsorted(idex,:,:);
         D2.warped_time = WARPED_TIME;
 
+        % smooth data:
+        C = downsample(smoothdata(D2.song_w.^2,2,'gaussian',1000)',1000)';
+        % find outliers:
+        TF = isoutlier(C(:,10:40),1);
+        I = find(mean(TF')<0.3);
+        
+        D2.song = D2.song(I,:);
+        D2.song_r = D2.song_r(I,:);
+        D2.song_w = D2.song_w(I,:);
+        D2.unsorted = D2.unsorted(I,:,:);
+        D2.warped_time = WARPED_TIME{I};
+        
+        
 disp(' Getting Gcon')
 %[Gconsensus3,f,t] = CY_Get_Consensus(WARPED_audio,fs);
 %D2.Gcon = Gconsensus3;
