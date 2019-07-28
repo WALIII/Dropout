4264f5557e22c57fafe7e9d26575a3e17ba9498b
WALIII <bliberti@bu.edu>
2019-07-27 17:58:23 -0700

    update analysis functions

--- legacy/FS_Spectrogram.m
+++ legacy/FS_Spectrogram.m
@@ -4,7 +4,7 @@ function [IMAGE, T,F]= FS_Spectrogram(mic_data,fs);
 
 
 
-[b,a]=ellip(5,.2,80,[500]/(fs/2),'high');
+[b,a]=ellip(5,.2,80,[100]/(fs/2),'high');
 [IMAGE,F,T] = fb_pretty_sonogram(filtfilt(b,a,mic_data./abs(max(mic_data))),fs,'low',2.9,'zeropad',0);
 colormap(hot)
 imagesc(T,F,log(abs(IMAGE)+1e+2));set(gca,'YDir','normal');
