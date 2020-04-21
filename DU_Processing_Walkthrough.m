97bc5e9a12ce6e1af30222fd11a46c050878f966
WALIII <bliberti@bu.edu>
2020-04-20 21:15:59 -0700

    save data

--- DU_Processing_Walkthrough.m
+++ DU_Processing_Walkthrough.m
@@ -7,7 +7,13 @@ mkdir('processed2');
 cd('processed2');
 metadata.processed_FN = pwd;
 
+% save data
+save('mov_data.mat','out','metadata');
 
+% clear major memory load:
+clear out
+
+% Motion correction
 metadata.moco.itter = 1;
 metadata.moco.bin_width = 200;
 CNMFe_align(out.mov_data,metadata)
@@ -15,7 +21,7 @@ CNMFe_align(out.mov_data,metadata)
 nam = './Motion_corrected_Data.mat'
 
 metadata.cnmfe.min_corr = 0.8;     % minimum local correlation for a seeding pixel
-metadata.cnmfe.min_pnr = 10;       % minimum peak-to-noise ratio for a seeding pixel
+metadata.cnmfe.min_pnr = 20;       % minimum peak-to-noise ratio for a seeding pixel
 % 2. Source extraction
 CNMFe_extract2(nam,'metadata',metadata);
 
